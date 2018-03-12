1a37ee7e030022dc899d7a2ac0f4541ec19604f6
jiaolong.wu <jiaolong.wu@baifendian.com>
2018-03-12 19:34:44 +0800

    修改wbepack配置

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -122,6 +122,8 @@ module.exports = {
         ],
         include: paths.appSrc,
       },*/
+
+
       {
         // "oneOf" will traverse all following loaders until one will
         // match the requirements. When no loader matches it will fall
@@ -149,7 +151,12 @@ module.exports = {
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
               cacheDirectory: true,
-            },
+                plugins: [
+                    ['import', { libraryName: 'antd', style: 'css' }],
+                ],
+            }
+
+
           },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,11 @@
 import React,{ Component } from 'react'
 import styles from './Login.less'
 import { observer,inject } from 'mobx-react'
-
+import { Button } from 'antd';
 @inject('loginStore') @observer
 class Login extends Component {
      render(){
-         return (<div>我是登陆页面</div>)
+         return (<div>我是登陆页面<Button type="primary">Button</Button></div>)
      }
 }
 export default Login
